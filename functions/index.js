aade5f340f180416c56e79bc63490af4c1774d8c
lukabuz <buzaladzeluka@gmail.com>
2019-10-08 01:08:04 -0400

    added readme, updated gitignore, cleaned up index.js for cf

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,4 @@ admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
 });
 
-//import custom classes
-var Lighter = require("./Objects/Lighter.js");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 exports.helloWorld = functions.https.onRequest((request, response) => {});
