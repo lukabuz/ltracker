0f4cdff666a06dcddd41470d7cc76e703f877b0b
qardpeet <patplaysg@gmail.com>
2019-10-10 14:10:19 +0400

    Started project -added .prettierrc -added fonts -cleaned up the react boilerplate -made a static logic screen

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import LoginScreen from './Components/LoginScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    isLoggedIn: false,
+    username: '',
+    password: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleLogin = e => {
+    this.setState(state => ({
+      isLoggedIn: !state.isLoggedIn,
+    }));
+  };
+
+  render() {
+    if (!this.state.isLoggedIn)
+      return (
+        <LoginScreen
+          handleInput={this.handleChange}
+          handleLogin={this.handleLogin}
+          username={this.state.username}
+          password={this.state.password}
+        />
+      );
+    return <div>App</div>;
+  }
 }
 
 export default App;
---
+++ web/src/Components/LoginScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export class LoginScreen extends Component {
+  render() {
+    return (
+      <div className="login-screen">
+        <div className="login-wrapper">
+          <h1 class="logo">Ltracker</h1>
+          <input
+            placeholder="username"
+            name="username"
+            type="text"
+            onChange={this.props.handleInput}
+            value={this.props.username}
+          />
+          <input
+            placeholder="password"
+            name="password"
+            type="password"
+            onChange={this.props.handleInput}
+            value={this.props.password}
+          />
+          <button className="btn" onClick={this.props.handleLogin}>
+            SIGN IN
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginScreen;
