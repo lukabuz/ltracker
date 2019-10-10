13dc47c301f232d798914ea545ed4c9d2f6463ac
lukabuz <buzaladzeluka@gmail.com>
2019-10-10 17:41:12 -0400

    changed db to realtime database. started working on functions for adding lighters, adding users, and authenticating users

--- functions/data.js
+++ functions/data.js
@@ -1,71 +1,167 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("../key.json");
+// const admin = require("firebase-admin");
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://ltracker-f226f.firebaseio.com"
-});
+// let serviceAccount = require("../key.json");
 
-const db = admin.firestore();
+// //initialize firebase db
+// admin.initializeApp({
+// 	credential: admin.credential.cert(serviceAccount),
+// 	databaseURL: "https://ltracker-f226f.firebaseio.com/"
+// });
 
-setDoc = (docRef, data) => {
-	docRef = db.doc(docRef);
+module.exports = class DataInterface {
+	constructor(db) {
+		this.db = db;
+		this.ref = db.ref();
+	}
+	// Create lighter
+	addLighter(number, color, description, added_by) {
+		let lighterData = {
+			number: number,
+			color: color,
+			description: description,
+			date_added: Date.now(),
+			lost_by: null,
+			added_by: added_by,
+			current_owner: added_by
+		};
 
-	let setData = docRef.set(data);
-};
+		let lightersRef = this.ref.child("lighters");
+		lightersRef.child(number).set(lighterData);
 
-getDoc = async docRef => {
-	return new Promise(resolve => {
-		docRef = db.doc(docRef);
-		let getDoc = docRef
-			.get()
-			.then(doc => {
-				if (!doc.exists) {
-					console.log("No such document!");
-				} else {
-					resolve(doc.data());
-				}
-			})
-			.catch(err => {
-				console.log("Error getting document", err);
+		this.createTransaction(added_by, number);
+
+		let usersRef = this.ref.child("users");
+		usersRef
+			.child(added_by)
+			.child("lighters_added")
+			.transaction(function(lighters_added) {
+				return (lighters_added || 0) + 1;
 			});
-	});
+	}
+
+	// Create transaction for lighter
+	createTransaction(given_to, number) {
+		let lighterRef = this.ref.child("lighters").child(number);
+		let ledger = lighterRef.child("ledger");
+		let date = Date.now();
+		ledger.child(date).set({
+			given_to: given_to,
+			date: date
+		});
+	}
+
+	// Report lighter loss
+	reportLighterLoss(number) {
+		// Update user entry and increment loss
+		let lighterRef = this.ref.child("lighters").child(number);
+		let ledger = lighterRef.child("ledger");
+		let usersRef = this.ref.child("users");
+		ledger.on("value", function(snapshot) {
+			let lost_by = snapshot.val();
+			let largest_key = 0;
+			for (var key in lost_by) {
+				if (key > largest_key) {
+					largest_key = key;
+				}
+			}
+			lost_by = lost_by[largest_key].given_to;
+
+			usersRef
+				.child(lost_by)
+				.child("lighters_lost")
+				.transaction(function(lighters_lost) {
+					return (lighters_lost || 0) + 1;
+				});
+
+			lighterRef.child("lost_by").set(lost_by);
+		});
+	}
+	getCreditScore(user) {
+		return new Promise((resolve, reject) => {
+			let userRef = this.ref.child("users").child(user);
+			userRef
+				.on("value", function(snapshot) {
+					let user = snapshot.val();
+					resolve(2 + user.lighters_added - user.lighters_lost);
+				})
+				.catch(err => {
+					resolve(0);
+				});
+		});
+	}
+	checkIfUserExists(user) {
+		return new Promise(resolve => {
+			let usersRef = this.ref.child("users").child(user);
+			usersRef.once("value", function(snapshot) {
+				if (snapshot.val() == null) {
+					resolve(true);
+				} else {
+					resolve(false);
+				}
+			});
+		});
+	}
+	checkIfLighterExists(number) {
+		return new Promise(resolve => {
+			let lightersRef = this.ref.child("lighters");
+			lightersRef.once("value", function(snapshot) {
+				if (snapshot.hasChild(number)) {
+					resolve(true);
+				} else {
+					resolve(false);
+				}
+			});
+		});
+	}
+	createUser(username, passwordHash) {
+		let usersRef = this.ref.child("users");
+		usersRef.child(username).set({
+			name: username,
+			lighters_added: 0,
+			lighters_lost: 0,
+			passwordHash: passwordHash
+		});
+	}
+	async getUserPasswordHash(username) {
+		return new Promise(resolve => {
+			let userRef = this.ref.child("users").child(username);
+			userRef.on("value", function(snapshot) {
+				let user = snapshot.val();
+				resolve(user.passwordHash);
+			});
+		});
+	}
 };
 
-addLighter = (number, color, description, added_by) => {
-	let lighterData = {
-		number: number,
-		color: color,
-		description: description,
-		date_added: Date.now(),
-		lost_by: null,
-		added_by: db.doc("users/" + added_by)
-	};
-
-	setDoc("lighters/" + number, lighterData);
-
-	createTransaction(added_by, number);
-
-	let userRef = db.doc("users/" + added_by);
-
-	let updateUser = userRef.update({
-		lighters_added: admin.firestore.FieldValue.increment(1)
-	});
-};
-
-createTransaction = (given_to, lighter_number) => {
-	let docRef = db.collection("lighters/" + lighter_number + "/ledger").doc();
-	let setTransaction = docRef.set({
-		given_to: db.doc("users/" + given_to),
-		date: Date.now()
-	});
-};
+// let dataProvider = new DataInterface(admin.database());
 
 // Add a lighter
 //
-// addLighter(3, "guava", "test", "Luka");
 
-// Get a lighter
-// getDoc("lighters/" + 3).then(res => {
+// dataProvider.addLighter(1, "red", "test", "Luka");
+// dataProvider.addLighter(2, "blue", "test", "Sam");
+// dataProvider.addLighter(3, "orange", "test", "Liam");
+// dataProvider.addLighter(4, "white", "test", "Annie");
+
+// Get a list of all lighters
+//
+// getAllLighters().then(res => {
 // 	console.log(res);
 // });
+
+// Check if a lighter exists
+//
+// checkDoc("lighters/3").then(res => {
+// 	console.log(res);
+// });
+
+// Report the loss of a lighter
+//
+// dataProvider.reportLighterLoss(4);
+
+// dataProvider.createTransaction("Luka", 4);
+// dataProvider.createTransaction("Sam", 4);
+// dataProvider.createTransaction("Liam", 4);
+// dataProvider.createTransaction("Luka", 2);
+// dataProvider.createTransaction("Liam", 2);
+// dataProvider.createTransaction("Annie", 2);
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,123 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const validator = require("html-request-validator");
+const hash = require("object-hash");
 
 let serviceAccount = require("../key.json");
+const DataInterface = require("./data.js");
 
 //initialize firebase db
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://ltracker-f226f.firebaseio.com/"
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {});
+class Auth {
+	constructor(username, password, dataProvider) {
+		this.username = username;
+		this.dataProvider = dataProvider;
+		this.password = password;
+		this.authenticated = false;
+		this.checkIfUserExists(username);
+	}
+	async checkIfUserExists(username) {
+		this.userExists = await dataProvider.checkIfUserExists(username);
+	}
+	register() {
+		let hash = this.createHash(this.password);
+		this.dataProvider.createUser(this.username, hash);
+	}
+	async authenticate() {
+		return new Promise(async resolve => {
+			let userHash = await dataProvider.getUserPasswordHash(this.username);
+			let authenticated = this.compareHash(this.password, userHash);
+			this.authenticated = authenticated;
+			resolve(authenticated);
+		});
+	}
+	createHash(password) {
+		return hash(password);
+	}
+	compareHash(password, hashToCompare) {
+		return hash(password) == hashToCompare;
+	}
+}
+
+let dataProvider = new DataInterface(admin.database());
+
+exports.logIn = functions.https.onRequest(async (request, response) => {
+	let errors = validator.validate(request.body, [
+		{ variable: "username", variableText: "username", min: 0, max: 30 },
+		{ variable: "password", variableText: "password", min: 6, max: 1000 }
+	]);
+
+	if (errors.length !== 0) {
+		response.json({ status: "error", errors: errors });
+	}
+
+	let auth = new Auth(
+		request.body.username,
+		request.body.password,
+		dataProvider
+	);
+
+	if (auth.userExists) {
+		let authenticated = await auth.login();
+		if (authenticated) {
+			response.json({ status: "success" });
+		} else {
+			response.json({
+				status: "error",
+				errors: ["Wrong Password."]
+			});
+		}
+	} else {
+		response.json({
+			status: "error",
+			errors: ["User does not exist."]
+		});
+	}
+});
+
+exports.createUser = functions.https.onRequest((request, response) => {
+	let errors = validator.validate(request.body, [
+		{ variable: "username", variableText: "username", min: 0, max: 30 },
+		{ variable: "password", variableText: "password", min: 6, max: 1000 }
+	]);
+
+	if (errors.length !== 0) {
+		response.json({ status: "error", errors: errors });
+	}
+
+	let auth = new Auth(
+		request.body.username,
+		request.body.password,
+		dataProvider
+	);
+
+	auth.register();
+
+	response.json({ status: "success" });
+});
+
+exports.createLighter = functions.https.onRequest((request, response) => {
+	let errors = validator.validate(request.body, [
+		{ variable: "number", variableText: "number", min: 0, max: 5000 },
+		{ variable: "color", variableText: "color", min: 2, max: 10 },
+		{ variable: "description", variableText: "description", min: 10, max: 50 },
+		{ variable: "added_by", variableText: "user", min: 0, max: 999 }
+	]);
+
+	if (errors.length !== 0) {
+		response.json({ status: "error", errors: errors });
+	}
+
+	dataProvider.addLighter(
+		request.body.number,
+		request.body.color,
+		request.body.description,
+		request.body.added_by
+	);
+
+	response.json({ status: "success" });
+});
